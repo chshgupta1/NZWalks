fb0e2c44be0e5aa0859d0d968bc4e8881b6b3c96
shubham <ch.shgupta.1@gmail.com>
2022-09-22 11:51:07 +0530

    End of Section2

---
+++ NZWalks/NZWalks.ApI/Models/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.ApI.Models.Domain
+{
+    public class Region
+    {
+        public Guid ID { get; set; }
+        public String Code { get; set; }
+        public String Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        //Navigation Property
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.ApI/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.ApI.Models.Domain
+{
+    public class Walk
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation Property
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
