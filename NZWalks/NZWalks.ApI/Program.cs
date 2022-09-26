c62dec6e4e46a74d1e3a2eb09a9bcfa0ce438514
Shubham Kumar Gupta <shua1kor@bosch.com>
2022-09-26 11:49:59 +0530

    End of Section3

--- NZWalks/NZWalks.ApI/Program.cs
+++ NZWalks/NZWalks.ApI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.ApI.Data;
+using NZWalks.ApI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,10 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnection"));
 });
+
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
---
+++ NZWalks/NZWalks.ApI/Repositories/IRegionRepository.cs
@@ -0,0 +1,9 @@
+﻿using NZWalks.ApI.Models.Domain;
+
+namespace NZWalks.ApI.Repositories
+{
+    public interface IRegionRepository
+    {
+        Task<IEnumerable<Region>> GetAllAsync();
+    }
+}
